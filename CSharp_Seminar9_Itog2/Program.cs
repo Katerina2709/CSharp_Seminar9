572c0f14b6f67d0d56d1716e5b7383e7dc0f48d0
Olga19bon <slizkovaev@mail.ru>
2023-03-02 02:12:44 +0300

    Решение задачи N 66

---
+++ CSharp_Seminar9_Itog2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// ввод числовых данных с консоли
+int ConsoleInputInt (string message)
+{
+    Console.Write(message);
+    int option = Convert.ToInt32(Console.ReadLine());
+    return option;
+}
+
+//  сумма натуральных элементов в промежутке от M до N
+int SumNaturalNumbs (int m, int n)
+{
+    if (m == n)  return n;
+    return m + SumNaturalNumbs (m + 1, n);
+}
+
+int numberM = ConsoleInputInt ("Введите натуральное число M: ");
+int numberN = ConsoleInputInt ("Введите натуральное число N: ");
+if (numberM <= 0 || numberN <= 0)
+{
+    Console.Write("Оба числа (M и N) должны быть натуральными");
+}
+else
+{
+    Console.Write ($"M = {numberM}; N = {numberN} -> {SumNaturalNumbs (numberM, numberN)}");
+}
